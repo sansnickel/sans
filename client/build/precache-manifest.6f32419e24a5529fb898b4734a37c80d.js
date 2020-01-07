7d2c40a5a4cb7d1db1c4da16f25939e5922e3b26
Nicholas Leung <nleungky@gmail.com>
2020-01-06 19:30:55 -0800

    build

---
+++ client/build/precache-manifest.6f32419e24a5529fb898b4734a37c80d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328886cb5bcd838e4f117ae401c41343",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73c8c94bae56dde13e5a",
+    "url": "/static/css/main.43847765.chunk.css"
+  },
+  {
+    "revision": "3e72d1ef21c6844cacf1",
+    "url": "/static/js/2.b5647881.chunk.js"
+  },
+  {
+    "revision": "73c8c94bae56dde13e5a",
+    "url": "/static/js/main.59d270fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "48c2d1b56cda3fef4edefb823905d87e",
+    "url": "/static/media/Dictionary.48c2d1b5.png"
+  },
+  {
+    "revision": "059ada6006a1eca476309b6c5a1589d0",
+    "url": "/static/media/League.059ada60.png"
+  },
+  {
+    "revision": "0d0345f662a757ef17eef497fa8e83fc",
+    "url": "/static/media/Loading.0d0345f6.gif"
+  },
+  {
+    "revision": "f0269dcc59f9d410be378932a4c299a0",
+    "url": "/static/media/Translink.f0269dcc.png"
+  },
+  {
+    "revision": "1e179f55ad86966ef00e4ee8ef97498f",
+    "url": "/static/media/Weather.1e179f55.png"
+  },
+  {
+    "revision": "f0499e49e769871f64a6aeccb830f50e",
+    "url": "/static/media/Wolfram.f0499e49.png"
+  }
+]);
